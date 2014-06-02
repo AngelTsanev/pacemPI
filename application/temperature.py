f6552761cc8458aaf3d1ae96b4beefc9d52ab64d
Angel Tsanev <archa40@gmail.com>
2014-06-02 18:43:12 +0300

    adds add_new_entry method

--- application/temperature.py
+++ application/temperature.py
@@ -12,3 +12,13 @@ def create_table(name):
         (time TEXT, temperature REAL)'''.format('table': name)
     cursor.execute(sql_request)
     connection.commit()
+
+def add_new_entry(table_name, data):
+    connection = lite.connect(location) 
+    cursor = connection.cursor()
+    current_time = time.strftime("%H:%M:%S") #23:59:23
+    sql_request = 'insert into {table} values (?,?)'.format('table':table_name)
+    cursor.execute(sql_request, [current_time, data])
+    connection.commit()
+
+
