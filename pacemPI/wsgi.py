e76616f8993c678f2a4633818f6fc60f2df62e0e
Angel Tsanev <archa40@gmail.com>
2014-06-28 13:06:19 +0300

    pictures for hardware installation

--- pacemPI/wsgi.py
+++ pacemPI/wsgi.py
@@ -16,7 +16,7 @@ application = get_wsgi_application()
 
 def on_startup():
    os.system("mkdir /tmp/stream 2> /dev/null")
-   os.system("raspistill -w 640 -h 480 -q 20 -o /tmp/stream/pic.jpg -tl 50 -t 9999999 2> /dev/null &")
+   os.system("raspistill -w 640 -h 480 -q 20 -o /tmp/stream/pic.jpg -tl 50 -t 99999999  2> /dev/null &")
 
    os.system("""sudo cp /home/pi/mjpg-streamer-code-182/mjpg-streamer/output_http.so /home/pi/mjpg-streamer-code-182/mjpg-streamer/input_file.so /usr/local/lib/ 2>/dev/null""")
 
